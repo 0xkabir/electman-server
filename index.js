1e9caeeff6491a51b50ff9f40895bc415a4f6d2e
0xkabir <kabir.sadiken1@gmail.com>
2022-11-11 20:45:01 +0600

    created api for an specific service

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ipnjwkc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,6 +25,13 @@ async function run(){
             const cursor = serviceCollection.find(query)
             const result = await cursor.limit(limit).toArray()
             response.send(result) 
+        })
+
+        app.get('/services/:id', async(request, response)=>{
+            const id = request.params.id
+            const query = {_id:ObjectId(id)}
+            const result = await serviceCollection.findOne(query)
+            response.send(result)
         })
     }
     finally {
